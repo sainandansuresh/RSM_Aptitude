5ed74ced6f5fea362daeac226a0239a88bb74b5f
Sai Nandan Suresh <71647633+sainandansuresh@users.noreply.github.com>
2022-01-19 12:59:54 +0530

    Create Train1.py

---
+++ Train1.py
@@ -0,0 +1,7 @@
+#1)A train moving at speed of 90 km/hr crosses a pole in 7 seconds.Find the length of the train.
+len_of_the_train = int(input('The speed of train in Km/hr: ' ))
+Total_time = int(input('Enter the time in seconds: '))
+Speed = len_of_the_train * (5/18)  #Speed is given in Km/hr so we converted it into m/s.
+Distance = len_of_the_train * Total_time
+print(Distance)
+
